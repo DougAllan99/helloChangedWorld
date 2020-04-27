3321d01f353494a70ad7ca1579be375b4fb9e131
DougAllan99 <50993701+DougAllan99@users.noreply.github.com>
2020-04-27 10:57:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print('Hey...welcome to the new world!')
 print("..wash your hands for 30 minutes with bleach and cough into the back of your neck.")
+print("...and don't touch your face with your toes...")
